89231b622b0db6f9e7aa00b2c7836bca710362c7
cmic <110636647+cmic2022@users.noreply.github.com>
2022-11-23 20:48:44 -0600

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -4,7 +4,7 @@ function mostrarScroll () {
     let scrollTop = document.documentElement.scrollTop;
     for (var i = 0; i < slide_animate.length; i ++ ) {
         let alturaAnimado = slide_animate[i].offsetTop;
-        if(alturaAnimado - 700 < scrollTop){
+        if(alturaAnimado - 1000 < scrollTop){
             slide_animate[i].style.opacity= 1;
             slide_animate[i].classList.add("slide-top")
         }
