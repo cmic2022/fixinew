eb93991af7d9c0e3d626c91798dccddedd8c0b0f
cmic <110636647+cmic2022@users.noreply.github.com>
2022-11-23 19:24:20 -0600

    Add files via upload

---
+++ js/animate1.js
@@ -0,0 +1,14 @@
+let slide_animate = document.querySelectorAll(".slide_animate");
+
+function mostrarScroll () {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i = 0; i < slide_animate.length; i ++ ) {
+        let alturaAnimado = slide_animate[i].offsetTop;
+        if(alturaAnimado - 600 < scrollTop){
+            slide_animate[i].style.opacity= 1;
+            slide_animate[i].classList.add("slide-top")
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
